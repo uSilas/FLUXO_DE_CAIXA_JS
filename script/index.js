eced284eaf7fa849ebb2cb2b740353349d5d73ac
uSilas <silas.oficial.prof@gmail.com>
2023-11-06 17:04:46 -0300

    tela de publicar iniciada

---
+++ script/index.js
@@ -0,0 +1,6 @@
+const xlsx = require("xlsx");
+const file = "./file_example_XLS_100.xlsx";
+const express = require("express");
+const app = express();
+const port = 2000;
+const _ = require("lodash");
